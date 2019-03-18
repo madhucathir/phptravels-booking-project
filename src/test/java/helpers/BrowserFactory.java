66445f34067321c3f190fb3d83a67cd42588fd5b
madhucathir <39658196+madhucathir@users.noreply.github.com>
2019-03-18 10:59:49 +0000

    Update BrowserFactory.java

--- src/test/java/helpers/BrowserFactory.java
+++ src/test/java/helpers/BrowserFactory.java
@@ -24,17 +24,7 @@ public class BrowserFactory {
 
     private static WebDriver createBrowserInstance() {
         if(RunCukesTest.remote){
-            if (RunCukesTest.browser.equalsIgnoreCase("chrome")) {
-                DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-                capabilities.setCapability("platform", "macOS 10.13");
-                capabilities.setCapability("version", "63.0");
-                String url = "http://sangajala55:04d71cd1-2ed2-41bd-9b41-c8376c26f0b5@ondemand.saucelabs.com:80/wd/hub";
-                try {
-                    return new RemoteWebDriver(new URL(url),capabilities);
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                }
-            } else if (RunCukesTest.browser.equalsIgnoreCase("firefox")) {
+             if (RunCukesTest.browser.equalsIgnoreCase("firefox")) {
                 DesiredCapabilities caps = DesiredCapabilities.firefox();
                 caps.setCapability("platform", "macOS 10.13");
                 caps.setCapability("version", "57.0");
@@ -48,7 +38,7 @@ public class BrowserFactory {
         }
         else {
             if (RunCukesTest.browser.equalsIgnoreCase("chrome")) {
-                System.setProperty("webdriver.chrome.driver","chromedriver");
+                System.setProperty("webdriver.chrome.driver","chromedriver.exe");
                 return new ChromeDriver();
             } else if (RunCukesTest.browser.equalsIgnoreCase("firefox")) {
                 return new FirefoxDriver();
